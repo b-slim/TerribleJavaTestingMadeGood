d76e8ea30f0136d17a05fb921f213b9d94eb8a45
Martin Allan Harris <martin.harris@tek-design.com>
2009-11-11 23:15:13 +0000

    Added initial rubbish test

---
+++ testing-made-good/src/main/java/com/tekdesign/lineutil/Main.java
@@ -0,0 +1,13 @@
+package com.tekdesign.lineutil;
+
+/**
+ * Run the line assembler.
+ * @author martinh
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print(StringPageMap.assembleLine("ManyOfMe | ",10));
+    }
+
+}
